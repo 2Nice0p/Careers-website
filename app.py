4dd5d155718e7b37d805d646f8c8a8ac81655390
2Nice0p <151602032+2Nice0p@users.noreply.github.com>
2024-02-15 09:40:06 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask       #from flask module we are using the Flask class
+from flask import Flask, render_template       #from flask module we are using the Flask class
 
 app = Flask(__name__)        #creating an instance of the Flask class
                              #app is an object name and the __name__ is inbulit name ........ use -  print(__name__) to check
@@ -6,7 +6,7 @@ app = Flask(__name__)        #creating an instance of the Flask class
 @app.route("/")              #creating a route for the app and @ is decorator
 
 def hello_world():           #creating a function
-  return "Hello JaanMan"
+  return render_template('home.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
